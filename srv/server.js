e53d0176983bd62b7b014aebeea2188ec9ee5218
Lucas Martins de Oliveira <lucastmartins.dev@gmail.com>
2026-06-04 15:50:08 -0300

    add initial setup for admin

--- srv/server.js
+++ srv/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const loginHandler   = require('./auth/login-handler');
 const authMiddleware = require('./auth/auth-middleware');
 const runSeed        = require('../scripts/seed-dev');
+const bootstrapAdmin = require('./bootstrap-admin');
 
 const WEBAPP_DIR      = path.resolve(__dirname, '../app/logestoque/webapp');
 const LOGIN_PAGE      = path.join(WEBAPP_DIR, 'login/index.html');
@@ -65,6 +66,14 @@ cds.on('bootstrap', (app) => {
             res.status(500).json({ message: `Erro: ${err.message}` });
         }
     });
+});
+
+cds.on('served', async () => {
+    try {
+        await bootstrapAdmin();
+    } catch (err) {
+        console.error('[bootstrap] Erro ao criar admin inicial:', err.message);
+    }
 });
 
 module.exports = cds.server;
